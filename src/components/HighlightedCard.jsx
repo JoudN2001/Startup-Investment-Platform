2aeb534e05938cccbe9671598437ad93d13c221d
JoudN <joud.kayale@gmail.com>
2026-04-14 01:19:57 +0300

    feat: build mobile and tablet layouts for Admin Dashboard
    - Create `HighlightedCard` component for tracking investment trends and growth metrics.
    - Build the Pending Approvals list section.
    - Develop `ProjectMiniCard` component with robust flexbox layout and implement list mapping.
    - Apply general UI/UX polish for smaller screens (desktop layout pending).

---
+++ src/components/HighlightedCard.jsx
@@ -0,0 +1,25 @@
+// ICON
+import { ChartNoAxesCombined, Landmark } from "lucide-react";
+
+const HighlightedCard = ({ title = "title", value = 0, trend }) => {
+  return (
+    <div className="relative overflow-hidden flex flex-col bg-primary rounded-2xl sm:rounded-5xl py-4 sm:py-5 px-5 sm:px-9 mt-6 lg:my-2">
+      {/* CARD INFO */}
+      <div className="z-10">
+        <span className="text-neutral-700 font-bold text-[10px] sm:text-xs tracking-widest font-secondary">
+          {title.toUpperCase()}
+        </span>
+        <h1 className="text-neutral text-4xl sm:text-5xl font-bold my-2 ">{value}</h1>
+        <div className="flex items-center gap-2.5 max-w-max text-tertiary text-xs sm:text-sm font-semibold bg-tertiary-100 px-2.5 py-1 sm:px-3 sm:py-1.5 mt-3 rounded-xl">
+          <ChartNoAxesCombined className="w-4 h-4 sm:w-5 sm:h-5 stroke-2" />
+          <span>{trend}</span>
+        </div>
+      </div>
+      {/* ===== CARD INFO ===== */}
+      {/* BACKGROUND ICON */}
+      <Landmark className="absolute z-0 -bottom-2 -right-4 w-28 h-auto sm:-bottom-2 sm:-right-7 sm:w-36 text-neutral-700 opacity-12" />
+    </div>
+  );
+};
+
+export default HighlightedCard;
---
+++ src/components/ProjectMiniCard.jsx
@@ -0,0 +1,46 @@
+export default function ProjectMiniCard({
+  thumbnail = "https://hips.hearstapps.com/hmg-prod/images/edc100123egan-002-6500742f5feb7.jpg?crop=0.9136xw:1xh;center,top&resize=1200:*",
+  status = "published",
+  title = "Architecture Office",
+  goal = "$1,200,000",
+  funded = 95,
+}) {
+  return (
+    <div className="flex bg-neutral max-[360px]:rounded-xl rounded-2xl p-3 max-[360px]:mt-2 mt-3 md:mt-0 shadow-xs">
+      {/* IMAGE */}
+      <div className="max-[360px]:hidden shrink-0 w-20 h-20 p-3 pl-1">
+        <img
+          className="w-full h-full object-cover rounded-lg"
+          src={thumbnail}
+          alt="Project Thumbnail"
+        />
+      </div>
+      {/* ===== IMAGE ===== */}
+
+      <div className="flex-1 min-w-0 mr-4">
+        {/* TITLE & STATUS */}
+        <div className="flex justify-between items-center">
+          <h1 className="text-lg truncate max-w-2/3 font-bold my-2">{title}</h1>
+          <span className="shrink-0 bg-neutral-900/60 text-neutral-400 backdrop-blur-xl max-[360px]:py-0.5 py-1 max-[360px]:px-1 px-1.5 text-[10px] font-semibold rounded-full">
+            {/* TODO: STATUS CIRCLE AND OTHER STATUS (DONE, PENDDING, REJECTED, ETC...) */}
+            {status.toUpperCase()}
+          </span>
+        </div>
+        {/* ===== TITLE & STATUS ===== */}
+
+        {/* PROGRESS BAR & FUND TARGET */}
+        <div className="w-full bg-neutral rounded-full max-[360px]:h-1 h-1.5 max-[360px]:mb-3 mb-4 mt-0.5 ">
+          <div
+            className="bg-linear-to-r from-tertiary-200 to-tertiary-600 max-[360px]:h-1 h-1.5 rounded-full"
+            style={{ width: `${funded}%` }}
+          ></div>
+        </div>
+        <div className="flex justify-between font-secondary max-[360px]:text-[11px] text-xs font-bold">
+          <h5 className="text-neutral-400">{`Target: ${goal}`}</h5>
+          <span>{`${funded}% Funded`}</span>
+        </div>
+        {/* ===== PROGRESS BAR & FUND TARGET ===== */}
+      </div>
+    </div>
+  );
+}
